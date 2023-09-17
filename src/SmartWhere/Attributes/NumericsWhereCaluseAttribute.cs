28049c655dd9644e58bff0008995a6f4dc494049
Baris Yerlikaya <b.yerlikaya@outlook.com>
2023-09-18 01:25:50 +0300

    NumericsWhereClauseAttribute added.

--- src/SmartWhere/Attributes/NumericsWhereCaluseAttribute.cs
+++ src/SmartWhere/Attributes/NumericsWhereCaluseAttribute.cs
@@ -1,6 +1,15 @@
-﻿namespace SmartWhere.Attributes
+﻿using SmartWhere.Enums;
+
+namespace SmartWhere.Attributes
 {
     public class NumericsWhereCaluseAttribute : WhereClauseAttribute
     {
+        public ComparisonOperator ComparisonOperator { get; set; }
+
+        public NumericsWhereCaluseAttribute(ComparisonOperator comparisonOperator, LogicalOperator logicalOperator = LogicalOperator.AND)
+            : base(logicalOperator) => ComparisonOperator = comparisonOperator;
+
+        public NumericsWhereCaluseAttribute(string propertyName, ComparisonOperator comparisonOperator, LogicalOperator logicalOperator = LogicalOperator.AND)
+            : base(propertyName, logicalOperator) => ComparisonOperator = comparisonOperator;
     }
 }
---
+++ src/SmartWhere/Enums/ComparisonOperator.cs
@@ -0,0 +1,30 @@
+﻿namespace SmartWhere.Enums
+{
+    public enum ComparisonOperator
+    {
+        /// <summary>
+        /// "="	Equal to
+        /// </summary>
+        Equal,
+        /// <summary>
+        /// "<>" Not equal to
+        /// </summary>
+        NotEqual,
+        /// <summary>
+        /// ">" Greater than 
+        /// </summary>
+        GreaterThan,
+        /// <summary>
+        /// ">=" Greater than or equal to
+        /// </summary>
+        GreaterThanOrEqual,
+        /// <summary>
+        /// "<" Less than
+        /// </summary>
+        LessThan,
+        /// <summary>
+        /// "<=" Less than or equal to
+        /// </summary>
+        LessThanOrEqual
+    }
+}
